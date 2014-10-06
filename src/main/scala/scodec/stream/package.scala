5e12eb5c961e0056159c58cc7ce947c6872f50ba
Michael Pilquist <mpilquist@gmail.com>
2014-10-06 19:56:33 -0400

    Add ability to convert a Process[Task, BitVector] in to a lazy BitVector

--- src/main/scala/scodec/stream/package.scala
+++ src/main/scala/scodec/stream/package.scala
@@ -15,9 +15,7 @@ package object stream {
   type StreamCodec[A] = scodec.stream.codec.StreamCodec[A]
   val StreamCodec = scodec.stream.codec.StreamCodec
 
-  /**
-   * Constructs a lazy `BitVector` by continuously reading from the
-   */
+  /** Constructs a lazy `BitVector` by continuously reading from the supplied process until it halts. */
   def toLazyBitVector(p: Process[Task, BitVector], bufferSize: Int = 100)(implicit S: Strategy): BitVector = {
     val queue = async.boundedQueue[BitVector](bufferSize)(S)
     p.to(queue.enqueue).onHalt { cause => Process.eval_(queue.close) }.run.runAsync(_ => ())
